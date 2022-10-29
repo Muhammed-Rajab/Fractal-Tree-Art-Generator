814ad033cd2ba14bb062e60521151c95b7024233
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-10-29 10:42:07 +0530

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(51);
+    stroke(255);
+    translate(200, height);
+    branch(100);
+}
+
+function branch(len) {
+    line(0, 0, 0, -len);
+}
